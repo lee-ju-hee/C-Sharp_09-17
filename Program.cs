5ffe3352847ebd34dfb201d5e1eebfd8abb1056a
lee-ju-hee <55524173+lee-ju-hee@users.noreply.github.com>
2020-09-21 21:48:46 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Report
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 2;
+
+            Console.WriteLine("두변수의 합: " + (x + y));
+            Console.WriteLine("두변수의 차: " + (x - y));
+            Console.WriteLine("두변수의 곱은: " + (x * y));
+            Console.WriteLine("두변수의 몫은: " + (x / y));
+            Console.WriteLine("두변수의 나머지는: " + (x % y));
+        }
+    }
+}
